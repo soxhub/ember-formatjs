a84be22b589518d5d877a66d26af66c233289517
Chris Manson <chris@manson.ie>
2021-11-18 16:27:43 +0000

    add basic broccoli plugin for converting lang files (with test)

---
+++ node-tests/lang-convert.js
@@ -0,0 +1,56 @@
+const { expect } = require('chai');
+
+const { createBuilder, createTempDir } = require('broccoli-test-helper');
+
+const LangConvert = require('../lib/lang-convert');
+
+let output;
+let input;
+
+async function buildFiles(files) {
+  input.write(files);
+
+  const subject = new LangConvert(input.path());
+  output = createBuilder(subject);
+
+  await output.build();
+
+  return output.read();
+}
+
+describe('Tag Generator', function () {
+  beforeEach(async () => {
+    input = await createTempDir();
+  });
+
+  afterEach(async () => {
+    try {
+      await input.dispose();
+    } finally {
+      // do nothing
+    }
+
+    if (output) {
+      await output.dispose();
+    }
+  });
+
+  it('convert the format of lang files to ember-intl supported files', async function () {
+    const files = await buildFiles({
+      'en-us.json': `{
+  "+/sgnu": {
+    "defaultMessage": "Assessment Templates"
+  },
+  "+5S38p": {
+    "defaultMessage": "Tasks are not in the same status. No bulk workflow actions can be performed."
+  }
+}`,
+    });
+
+    expect(files).to.have.property('en-us.json');
+    expect(files['en-us.json']).to.equal(`{
+  "+/sgnu": "Assessment Templates",
+  "+5S38p": "Tasks are not in the same status. No bulk workflow actions can be performed."
+}`);
+  });
+});
