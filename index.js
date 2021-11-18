20ef5e9414a7e0090882596006eb9c333d3f6b6f
Chris Manson <chris@manson.ie>
2021-11-18 17:13:52 +0000

    automatically convert lang files to translations

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 'use strict';
 
+const { join } = require('path');
+const LangConvert = require('./lib/lang-convert');
+
 module.exports = {
   name: require('./package').name,
+
+  treeForTranslations() {
+    let appPrefix = join(this.project.configPath(), '../..');
+
+    return new LangConvert(join(appPrefix, 'lang'));
+  },
 };
