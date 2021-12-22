c118ee6069d26022fb367b16415fe8dea3bd7a3a
Dave Laird <kiwiupover@gmail.com>
2021-12-22 10:50:50 -0800

    Code cleanup

--- index.js
+++ index.js
@@ -5,33 +5,26 @@ const LangConvert = require('./lib/lang-convert');
 const { addPlugin, hasPlugin } = require('ember-cli-babel-plugin-helpers');
 const Plugin = require.resolve('./lib/format-message-replace');
 
-
 module.exports = {
   name: require('./package').name,
 
   treeForTranslations() {
-    let appPrefix = join(this.project.configPath(), '../..');
+    const appPrefix = join(this.project.configPath(), '../..');
 
     return new LangConvert(join(appPrefix, 'lang'));
   },
 
-  included(par) {
+  included() {
     this._super.included.apply(this, arguments);
 
-    // inline _findHost
     let current = this;
     let app;
     do {
       app = current.app || app;
     } while (current.parent.parent && (current = current.parent));
 
-    // let idInterpolationPattern =
-    //   (app.options ?? {})['ember-formatjs']?.idInterpolationPattern ??
-    //   '[sha512:contenthash:base64:6]';
-    // let plugin = createPlugin(idInterpolationPattern);
-
-    if (!hasPlugin(par, Plugin)) {
-      addPlugin(par, Plugin);
+    if (!hasPlugin(app, Plugin)) {
+      addPlugin(app, Plugin);
     }
 
     // we can't use the setupPreprocessorRegistry() because there is no access to app.options
@@ -50,7 +43,7 @@ module.exports = {
   },
 
   _buildReplacePlugin(app) {
-    let KeyTransform = require('./replace-key-transform');
+    let KeyTransform = require('./lib/replace-key-transform');
 
     let idInterpolationPattern =
       (app.options ?? {})['ember-formatjs']?.idInterpolationPattern ??
