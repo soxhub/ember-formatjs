33d77530f7f20ef38110fd213383f744126b5432
Dave Laird <kiwiupover@gmail.com>
2021-12-26 14:18:09 -0800

    Better test coverage

--- node-tests/fixtures/format-message-replace/code.js
+++ node-tests/fixtures/format-message-replace/code.js
@@ -1,9 +1,57 @@
 import Controller from '@ember/controller';
 
-export default class LoginController extends Controller {
-	get translation() {
-		return this.intl.formatMessage({
-			defaultMessage: 'Max was not really here he didn\'t like it!',
+export default class AController extends Controller {
+	get message() {
+		return this.intl.formatMessage('The message is: message');
+	}
+
+	get messageName() {
+		return this.intl.formatMessage('The user is: {user}', {
+			name: 'Scott'
+		});
+	}
+
+	get messageDescription() {
+		return this.intl.formatMessage('The message is: message', {
+			description: 'this is a description',
+		});
+	}
+
+	get messageDescriptionWithArgs() {
+		return this.intl.formatMessage('The message is: {description}', {
+			description: "foo",
+		});
+	}
+
+	get messageIdWithArgs() {
+		return this.intl.formatMessage('The message id: {id}', {
+			id: "12345",
+		});
+	}
+
+	get messageValueArgsAndOptions() {
+		return this.intl.formatMessage('The user is: {name}', {
+			name: this.args.user.name
+		}, {
+			description: 'this is a description',
+		});
+	}
+
+	get messageValueArgsWithId() {
+		return this.intl.formatMessage('The user is: {name}, {description}', {
+			name: this.args.user.name,
+		}, {
+			description: 'this is a description',
+			id: 'login.message-description-with-id',
+		});
+	}
+
+	get messageValueArgsWithDescriptionAndOptionsDescription() {
+		return this.intl.formatMessage('The user is: {name}, {description}', {
+			name: this.args.user.name,
+			description: 'description for the message'
+		}, {
+			description: 'this is a description',
 		});
 	}
 }
--- node-tests/fixtures/format-message-replace/output.js
+++ node-tests/fixtures/format-message-replace/output.js
@@ -1,6 +1,47 @@
 import Controller from '@ember/controller';
-export default class LoginController extends Controller {
-  get translation() {
-    return this.intl.t('e8Nx8h');
+export default class AController extends Controller {
+  get message() {
+    return this.intl.t('2/U1G5');
+  }
+
+  get messageName() {
+    return this.intl.t('QxXsbh', {
+      name: 'Scott',
+    });
+  }
+
+  get messageDescription() {
+    return this.intl.t('auSKLo');
+  }
+
+  get messageDescriptionWithArgs() {
+    return this.intl.t('2FwN61', {
+      description: 'foo',
+    });
+  }
+
+  get messageIdWithArgs() {
+    return this.intl.t('vCPL33', {
+      id: '12345',
+    });
+  }
+
+  get messageValueArgsAndOptions() {
+    return this.intl.t('qfLPS1', {
+      name: this.args.user.name,
+    });
+  }
+
+  get messageValueArgsWithId() {
+    return this.intl.t('login.message-description-with-id', {
+      name: this.args.user.name,
+    });
+  }
+
+  get messageValueArgsWithDescriptionAndOptionsDescription() {
+    return this.intl.t('DR8H1S', {
+      name: this.args.user.name,
+      description: 'description for the message',
+    });
   }
 }
