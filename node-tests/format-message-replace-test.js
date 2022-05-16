2e414caed767a6a961dafc3c9136ac6f0432f5d9
Bartlomiej Dudzik <bartholomew.dudzik@gmail.com>
2022-05-16 20:26:23 +0200

    feat: add preserveWhitespace handling, create plugins with options

--- node-tests/format-message-replace-test.js
+++ node-tests/format-message-replace-test.js
@@ -1,9 +1,12 @@
 const { join } = require('path');
 const pluginTester = require('babel-plugin-tester').default;
-const formatMessageReplace = require('../lib/format-message-replace');
+const FormatMessageReplace = require('../lib/format-message-replace');
 
 pluginTester({
-  plugin: formatMessageReplace,
+  plugin: FormatMessageReplace({
+    idInterpolationPattern: '[sha512:contenthash:base64:6]',
+    preserveWhitespace: false,
+  }),
   pluginName: 'format message replace',
   fixtures: join(__dirname, 'fixtures'),
 });
